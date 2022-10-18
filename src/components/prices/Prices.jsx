d9f425afc14977bd265d86dca053c735f754d46d
Axl <axlsntz.dev@gmail.com>
2022-10-17 21:22:21 -0600

    add new updates

--- src/components/prices/Prices.jsx
+++ src/components/prices/Prices.jsx
@@ -3,6 +3,7 @@ import Pricing from '../pricing/Pricing'
 import basic from '../../assets/basic.jpg'
 import standard from '../../assets/standard2.jpg'
 import premium from '../../assets/premium.jpg'
+import navidad from '../../assets/arbol.jpg'
 import './prices.css'
 const PricingList = ({handleToggleTerms}) => {
   return (
@@ -16,18 +17,36 @@ const PricingList = ({handleToggleTerms}) => {
               title='Paquete Basico' 
               price='1790'
               details={['5 fotografias digitales', '5 fotografias impresas (mismas entregadas)', ' 30 minutos de sesion','Una galeria digital con 5 fotografias editadas y listas para compartir en redes sociales.']}/>
+             
               <Pricing 
               handleToggleTerms={handleToggleTerms}
               img={standard}
               title='Paquete Standard' 
               price='2190'
               details={['10 fotografias digitales', '10 fotografias impresas (mismas entregadas)', ' 1 hora de sesion','Una galeria digital con 10 fotografias editadas y listas para compartir en redes sociales.']}/>
+             
               <Pricing 
               handleToggleTerms={handleToggleTerms}
               img={premium}
               title='Paquete Premium' 
               price='2990'
               details={['15 fotografias digitales', '15 fotografias impresas (mismas entregadas)', ' 1 hora de sesion','Una galeria digital con 15 fotografias editadas y listas para compartir en redes sociales.', 'canva']}/>
+             
+              <Pricing 
+              especial={true}
+              handleToggleTerms={handleToggleTerms}
+              img={navidad}
+              title='Paquete Navideño A' 
+              price='2100'
+              details={['10 fotografias digitales', '10 fotografias impresas (mismas entregadas)', ' 1 hora de sesion','Set navideño']}/>
+             
+              <Pricing 
+              especial={true}
+              handleToggleTerms={handleToggleTerms}
+              img={navidad}
+              title='Paquete Navideño B' 
+              price='2700'
+              details={['15 fotografias digitales', '15 fotografias impresas (mismas entregadas)', ' 1 hora de sesion','Set navideño']}/>
         </div>
     </section>
   )
--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './pricing.css'
 import PricingModal from './PricingModal'
 
-const Pricing = ({handleToggleTerms,img,title, description = 'La sesion puede ser realizada en el estudio o en algun lugar de su preferencia.', price, details}) => {
+const Pricing = ({especial = false, handleToggleTerms,img,title, description = 'La sesion puede ser realizada en el estudio o en algun lugar de su preferencia.', price, details}) => {
     const [toggleModal, setToggleModal] = useState(false);
     const handleToggleModal = () => {
         setToggleModal(!toggleModal)
@@ -10,9 +10,11 @@ const Pricing = ({handleToggleTerms,img,title, description = 'La sesion puede se
   return (
    <div className="pricing__card">
     <div className="pricing__card-container">
-        {/* <img src={img} alt="sesion-one" />
-         */}
-         <div style={{backgroundImage:  `url(${img})`}} className="pricing__card-img"></div>
+
+        <div style={{backgroundImage:  `url(${img})`}} className="pricing__card-img"></div>
+        {especial && <div className="pricing__card-especial">
+            <span>Especial</span>
+        </div>}
         <div className="pricing__card-info">
             <h4 >{title}</h4>
             <span>{description}</span>
